a6a3b916d0ec60dfcacdd7c7526f9e47699c54ff
Sergio225 <s3rg10.martinez.roman@gmail.com>
2019-04-15 18:33:02 +0200

    Game finished

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -23,6 +23,7 @@ const config = {
 function preload()
 {
   this.load.image('ship', 'assets/spaceShips_001.png');
+  this.load.image('star', 'assets/star_gold.png');
 }
   
 function create()
@@ -44,14 +45,25 @@ function create()
       x: Math.floor(Math.random() * 700) + 50,
       y: Math.floor(Math.random() * 500) + 50,
       playerId: socket.id,
-      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+      input: {
+        left: false,
+        right: false,
+        up: false
+      }
     };
+
     // add player to server
     addPlayer(self, players[socket.id]);
     // send the players object to the new player
     socket.emit('currentPlayers', players);
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    // send the star object to the new player
+    socket.emit('starLocation', { x: self.star.x, y: self.star.y });
+    // send the current scores
+    socket.emit('updateScore', self.scores);
 
     socket.on('disconnect', function () {
       console.log('user disconnected');
@@ -62,11 +74,73 @@ function create()
       // emit a message to all players to remove this player
       io.emit('disconnect', socket.id);
     });
+
+    // when a player moves, update the player data
+    socket.on('playerInput', function (inputData) {
+      handlePlayerInput(self, socket.id, inputData);
+    });
+
+  });
+
+  this.scores = {
+    blue: 0,
+    red: 0
+  };
+   
+  this.star = this.physics.add.image(randomPosition(700), randomPosition(500), 'star');
+  this.physics.add.collider(this.players);
+   
+  this.physics.add.overlap(this.players, this.star, function (star, player) {
+    if (players[player.playerId].team === 'red') {
+      self.scores.red += 10;
+    } else {
+      self.scores.blue += 10;
+    }
+    self.star.setPosition(randomPosition(700), randomPosition(500));
+    io.emit('updateScore', self.scores);
+    io.emit('starLocation', { x: self.star.x, y: self.star.y });
   });
 
 }
   
-function update() {}
+function update() 
+{
+  this.players.getChildren().forEach((player) => {
+    const input = players[player.playerId].input;
+    if (input.left) {
+      player.setAngularVelocity(-300);
+    } else if (input.right) {
+      player.setAngularVelocity(300);
+    } else {
+      player.setAngularVelocity(0);
+    }
+   
+    if (input.up) {
+      this.physics.velocityFromRotation(player.rotation + 1.5, 200, player.body.acceleration);
+    } else {
+      player.setAcceleration(0);
+    }
+   
+    players[player.playerId].x = player.x;
+    players[player.playerId].y = player.y;
+    players[player.playerId].rotation = player.rotation;
+  });
+  this.physics.world.wrap(this.players, 5);
+  io.emit('playerUpdates', players);
+  
+}
+
+function randomPosition(max) {
+  return Math.floor(Math.random() * max) + 50;
+}
+
+function handlePlayerInput(self, playerId, input) {
+  self.players.getChildren().forEach((player) => {
+    if (playerId === player.playerId) {
+      players[player.playerId].input = input;
+    }
+  });
+}
 
 function addPlayer(self, playerInfo) {
   //self.physics.add.image to allow that game object to use the arcade physics
